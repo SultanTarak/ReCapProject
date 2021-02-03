bcc7f9b5264931aa2259b144fda49e98ce2122bd
SultanTarak <begumsultant@gmail.com>
2021-02-03 14:56:46 +0300

    Proje dosyasıAdd project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1, BrandId=1, ColorId=1, ModelYear=2019, DailyPrice=300, Description="BMW 1 Serisi"},
+                new Car{CarId=2, BrandId=1, ColorId=1, ModelYear=2017, DailyPrice=270, Description="Audi A3"},
+                new Car{CarId=3, BrandId=2, ColorId=1, ModelYear=2016, DailyPrice=150, Description="Renault Megane"},
+                new Car{CarId=4, BrandId=3, ColorId=1, ModelYear=2015, DailyPrice=400, Description="Mercedes GLA"},
+                new Car{CarId=5, BrandId=3, ColorId=1, ModelYear=2020, DailyPrice=850, Description="Porsche Panamera"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
