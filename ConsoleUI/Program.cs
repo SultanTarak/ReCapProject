93218489b6e66e1653cdede317cd87da06915264
SultanTarak <begumsultant@gmail.com>
2021-02-05 11:21:08 +0300

    Project files

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleUI
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description + " " + car.ModelYear + " Model " + "Günlük Kiralama Bedeli: " + car.DailyPrice);
+
             }
         }
     }
