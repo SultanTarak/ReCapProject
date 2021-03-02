40e755636575612e0635431b7df08b57e0affa69
SultanTarak <begumsultant@gmail.com>
2021-03-02 18:52:56 +0300

    13.Ders ödev

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,14 +9,26 @@ namespace Business.Constants
     {
         public static string Added = "Ekleme başarılı";
         public static string NameInvalid = "Eksik veya hatalı bilgi";
+
         public static string Updated = "Güncelleme başarılı";
         public static string UpdatedInvalid = "Güncelleme başarısız";
+        
         public static string Deleted = "Silme başarılı";
         public static string DeletedInvalid = "Silme başarısız";
+        
         public static string Listed = "Ürünler listelendi.";
+        
         public static string DailyPriceInvalid = "Birim fiyatı 0'dan büyük olmalıdır.";
+        
         public static string CarCountOfBrandError = "Bir markada en fazla 10 araç olabilir";
+        
         public static string CarNameAlreadyExists = "Aynı isimde başka bir araç mevcuttur";
+        
         public static string BrandLimitExceded= "Marka limiti aşıldığı için yeni araç eklenemiyor";
+
+        public static string CarRental = "Araç kiralandı";
+        
+        public static string CarImageLimitExceded = "Resim limiti aşıldığı için yeni resim eklenemiyor";
+        public static string CarImageNull = "Araç görüntüsü eklenmelidir";
     }
 }
